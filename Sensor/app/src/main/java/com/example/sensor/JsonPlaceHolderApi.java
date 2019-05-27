41df1a6dd4c841ecf1f845bdc3856ba8afad3b33
ahmad9272 <ahmadhalabi9272@gmail.com>
2019-05-27 09:20:33 +0200

    @Post

---
+++ Sensor/app/src/main/java/com/example/sensor/JsonPlaceHolderApi.java
@@ -0,0 +1,27 @@
+/*package com.example.sensor;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.POST;
+
+public interface JsonPlaceHolderApi {
+
+    @FormUrlEncoded
+    @POST("posts")
+    Call<Post> createPost(
+            @Field("locationManager") double locationManager,
+            @Field("locationListener") double locationListener,
+            @Field("body") String text
+
+    );
+
+
+
+
+
+}
+*/
---
+++ Sensor/app/src/main/java/com/example/sensor/Post.java
@@ -0,0 +1,23 @@
+/*package com.example.sensor;
+
+import android.widget.TextView;
+
+public class Post {
+    public TextView getTvLat() {
+        return tvLat;
+    }
+
+    public TextView getTvLong() {
+        return tvLong;
+    }
+
+    private TextView tvLat;
+
+    public Post(TextView tvLat, TextView tvLong) {
+        this.tvLat = tvLat;
+        this.tvLong = tvLong;
+    }
+
+    private TextView tvLong;
+}
+*/
