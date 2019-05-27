89fbe83983b2f146eb9445794cac962cb44437d8
ahmad9272 <ahmadhalabi9272@gmail.com>
2019-05-27 09:29:13 +0200

    Adding File

--- Sensor/app/src/main/java/com/example/sensor/MainActivity.java
+++ Sensor/app/src/main/java/com/example/sensor/MainActivity.java
@@ -88,6 +88,7 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
         locationManager.removeUpdates(this);
     }
 
+    //
     @Override
     protected void onDestroy() {
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
